8d1c8a967b56d25690ab85d5795d0c66a945ff30
nicholashanson <nicholashanson27@outlook.com>
2021-08-08 17:33:15 +0800

    Update atmega32_adding.asm

--- atmega32_adding.asm
+++ atmega32_adding.asm
@@ -1,10 +1,10 @@
               .EQU      SUM = 0x300
-              .ORG			00
-              LDI				R16, 0x25
-              LDI				R17, $34
-              LDI				R18, 0b00110001
-              ADD				R16, R17
-              LDI				R17, 11
-              ADD				R16, R18
-              STS				SUM, R16
-HERE:			    JMP				HERE
+              .ORG      00
+              LDI       R16, 0x25
+              LDI       R17, $34
+              LDI       R18, 0b00110001
+              ADD       R16, R17
+              LDI       R17, 11
+              ADD       R16, R18
+              STS       SUM, R16
+HERE:         JMP       HERE
